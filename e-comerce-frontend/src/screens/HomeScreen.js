5f2b7f9e1338d73487224bcd527a41efb38fabcb
3110manav <81803132+3110manav@users.noreply.github.com>
2022-10-27 22:34:06 +0530

    minor changes

--- e-comerce-frontend/src/screens/HomeScreen.js
+++ e-comerce-frontend/src/screens/HomeScreen.js
@@ -26,36 +26,32 @@ const HomeScreen = () => {
   return (
     <div >
       <div className="intro0">
-       
+
         <div className="head w3-container">
           <h1 id="title"><u>SAKSHAM</u></h1>
           <h3>A Marketplace for Specially Abled</h3>
 
           <p id="info" className="w3-left w3-animate-left">
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Veritatis beatae, mollitia dolore quas cumque,
-            dolorum reiciendis quaerat quia repellat doloremque non, distinctio eius voluptas. Velit quibusdam
-            doloribus
-            molestias perspiciatis voluptates, expedita, et, repudiandae blanditiis ad quas enim consectetur nam!
-            Atque,
-            culpa tenetur. Aliquid pariatur veritatis quia in cum animi porro.
+            We are an e-commerce platform designed exclusively to sell amazing handicraft products made by very talented, specially abled artisans. This provides a source of income for these artisans and you can buy exquisite products at a very reasonable price.
+            So SHOP NOW !!!
+
           </p>
         </div>
       </div>
 
       <div className="intro1">
         <div className="rect1 reveal">
-          <img id="ngo1" src="ngo1a.jpg" alt="Prarambh NGO Image"/>
+          <img id="ngo1" src="ngo1a.jpg" alt="Prarambh NGO Image" />
         </div>
         <div className="rect2 reveal">
-            <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos nostrum commodi vitae! Maxime, vel
-                tenetur
-                <br/><br/>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus, quo fugiat. Quibusdam cum quasi
-                dolore iure, animi sit, obcaecati omnis et optio aliquid asperiores possimus!
-            </p>
+          <p>
+          <i>Success Stories ..</i><br/><br/>
+"Jayant Prakash is a wheelchair user from Odissa who is a known artist as well. 
+            <br/><br/>
+            Apart from working on beautiful mural paintings, Jayant makes umbrellas, paper pens and straw baskets for a living. Mural paintings are his specialty. Jayant has done over 30 mural paintings till date, and all of them have been sold off too. A disability has not stopped this youngster from living an independent and happy life."
+        </p>
         </div>
-    </div>
+      </div>
     </div>
   )
 }
